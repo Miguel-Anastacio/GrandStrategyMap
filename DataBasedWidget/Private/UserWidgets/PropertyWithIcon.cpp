e67065ffd792fab3d39bc11c194e0ac936a7dc0f
MiguelAnastacio <78035869+Miguel-Anastacio@users.noreply.github.com>
2025-09-27 16:42:21 +0100

    Squashed 'Plugins/InteractiveMap/Source/PropDispGen/' changes from 15a58f3..b513f2c
    b513f2c fix unreal engine version 5.5
    f9b21fc cleanup test module
    9867a25 Update README.md
    9b9d1a9 Update README.md
    6682069 setup
    REVERT: 15a58f3 fix unreal engine version 5.5
    REVERT: 86a34ae Merge pull request #1 from Miguel-Anastacio/branch/5.5
    REVERT: 3377ac4 Change build
    REVERT: f9c2d74 multi selection
    REVERT: 5fa1ffe add clear selection to list
    REVERT: 0cee683 struct list temporary changes for map
    REVERT: 570233d log progress
    REVERT: 2573777 check struct field function, improve InstancedStructList for BP structs, asset save unique names
    REVERT: 73a7295 5.5 build files
    REVERT: 1dcbc20 add license
    REVERT: 519b2dd fix copyright
    REVERT: f578ab9 Merge branch 'master' of https://github.com/Miguel-Anastacio/UtilityPlugin into HEAD
    REVERT: fe8f562 Compatible with 5.4 and 5.5, comments
    REVERT: 1f50823 Create README.md
    REVERT: 6d47e61 Make editor functions callable in blueprints
    REVERT: d367f5d test
    REVERT: edbd838 Test
    REVERT: 83f8dd2 Correct dir
    REVERT: c08e7c0 Inital Commit
    
    git-subtree-dir: Plugins/InteractiveMap/Source/PropDispGen
    git-subtree-split: b513f2cb98557bd3c2c56041d557b788b545c86c

---
+++ DataBasedWidget/Private/UserWidgets/PropertyWithIcon.cpp
@@ -0,0 +1,38 @@
+// Copyright 2024 An@stacioDev All rights reserved.
+
+#include "UserWidgets/PropertyWithIcon.h"
+#include "BlueprintLibrary/ADStructUtilsFunctionLibrary.h"
+#include "BlueprintLibrary/PropertyUtilityFunctionLibrary.h"
+#include "Components/RichTextBlock.h"
+#include "Components/Image.h"
+#include "Engine/Texture2D.h"
+
+void UWPropGenPropertyWithIcon::SetValues(const FText& LabelText, const FText& ValueText) const
+{
+	if(Label)
+		Label->SetText(LabelText);
+	if(Text)
+		Text->SetText(ValueText);
+}
+
+void UWPropGenPropertyWithIcon::SetIcon(UTexture2D* NewIcon) const
+{
+	if(Icon && NewIcon)
+	{
+		Icon->SetBrushFromTexture(NewIcon);
+	}
+}
+
+void UWPropGenPropertyWithIcon::InitFromData(const FName& PropertyName,const UStruct* ClassType,  const void* Data) const
+{
+	const FProperty* Property = UAtkStructUtilsFunctionLibrary::FindPropertyByDisplayName(ClassType, PropertyName);
+	const FString ValueText = UAtkStructUtilsFunctionLibrary::GetPropertyValueAsString(Property, Data);
+	SetValues(FText::FromName(PropertyName), FText::FromString(ValueText));
+
+	SetIcon(UAtkPropertyUtilityFunctionLibrary::GetPropertyAs<UTexture2D>(Property, UTexture2D::StaticClass(), Data));
+}
+
+void UWPropGenPropertyWithIcon::NativeOnListItemObjectSet(UObject* ListItemObject)
+{
+	IUserObjectListEntry::NativeOnListItemObjectSet(ListItemObject);
+}
