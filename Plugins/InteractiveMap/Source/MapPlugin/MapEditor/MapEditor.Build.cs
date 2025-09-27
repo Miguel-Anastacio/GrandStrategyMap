44621ac2dfb8b899acc5c58c8bae9e20bd8dbc91
MiguelAnastacio <78035869+Miguel-Anastacio@users.noreply.github.com>
2025-09-27 15:45:40 +0100

    correct file structure for build postprocess

---
+++ Plugins/InteractiveMap/Source/MapPlugin/MapEditor/MapEditor.Build.cs
@@ -0,0 +1,73 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+using System.IO;
+using UnrealBuildTool;
+
+public class MapEditor : ModuleRules
+{
+	public MapEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+            }
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			
+            }
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"ApplicationCore",
+				"RHI",
+				"RenderCore",
+				"UtilityModule",
+				"UtilityModuleEditor",
+				"StructUtils",
+				"SharedModule", 
+				"Engine",
+				"MapGeneratorLibrary"
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+                "Slate",	
+				"SlateCore",
+                "UMG",
+				"Json",
+				"JsonUtilities",
+				"AssetTools",
+				"AssetDefinition",
+				"AdvancedPreviewScene",
+				"ToolMenus",
+				"UnrealEd",
+				"EditorStyle",
+				"WorkspaceMenuStructure",
+				"PropertyEditor",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
