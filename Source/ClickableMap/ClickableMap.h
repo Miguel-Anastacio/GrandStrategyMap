588214f664abf56950a05281f822e3c6e668dfc7
Miguel-Anastacio <miguelmachado1000@gmail.com>
2026-03-12 23:10:13 +0000

    Misc files edited: docs, notes

--- Source/ClickableMap/ClickableMap.h
+++ Source/ClickableMap/ClickableMap.h
@@ -4,7 +4,7 @@
 
 #include "CoreMinimal.h"
 
-class FShaderBitsModule : public IModuleInterface
+class FClickableMapGameModule : public IModuleInterface
 {
 public:
 	virtual void StartupModule() override;
